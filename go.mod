d60163c0b6be73240f1ecff47755229af71463a6
Umang Kumar Suarnkar <umang@Adityas-MacBook.local>
2024-07-09 15:33:54 +0530

    my first  commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/umang-soni/golang.git
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("fb wfdbghorld")
+}
+
+// input
+// for loop
+//while loop
+
